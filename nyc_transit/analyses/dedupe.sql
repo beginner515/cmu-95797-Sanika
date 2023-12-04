f1c17995195e19de25ad5747bdde1c14ed65be3e
Mythinkbook\Sanika <sanika.hadatgune.sh@gmail.com>
2023-12-03 20:11:28 -0500

    Survived week 5

--- nyc_transit/analyses/dedupe.sql
+++ nyc_transit/analyses/dedupe.sql
@@ -1,11 +1,5 @@
-SELECT
-    insert_timestamp,
-    event_id,
-    event_type,
-    user_id,
-    event_timestamp
-FROM events
-QUALIFY ROW_NUMBER() OVER (
-    PARTITION BY event_id, event_type, user_id, insert_timestamp
-    ORDER BY event_timestamp DESC
-) = 1;
+select * from events
+qualify row_number()
+over (partition by event_id
+order by insert_timestamp desc)
+= 1
